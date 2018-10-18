74ab809518c1bdfa4e5d214b7e36051250fc3e03
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-18 01:03:37 -0400

    Saving ggmap3.R

--- ggmap3.R
+++ ggmap3.R
@@ -8,4 +8,5 @@ ggmap3 <- ggplot(mergeDF, aes(map_id = stateName))
 
 ggmap3 <- ggmap3 + geom_map(map = us, aes(fill=Murder))+ scale_fill_gradient(low="white",high="red")
 
+ggmap3 <- ggmap3 + expand_limits(x=us$long, y=us$lat) + coord_map()
 
