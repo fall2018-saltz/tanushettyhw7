aff217dd19194d5e5f6b4b9f1fb7ba2e5a24fc81
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-18 00:44:21 -0400

    Saving ggMap1.R

--- ggMap1.R
+++ ggMap1.R
@@ -8,6 +8,6 @@ us<- map_data("state")
 ggmap1 <- ggplot(mergeDF, aes(map_id = stateName)
 
 
-ggmap1 <- ggmap1 + geom_map(map=us, aes(fill=stateName) + expand_limits(x = mergeDF$x, y=mergeDF$y) + coord_map() + ggtitle("Map of US based on state area")
+ggmap1 <- ggmap1 + geom_map(map=us, aes(fill=stateName) + expand_limits(x = x, y=y) + coord_map() + ggtitle("Map of US based on state area"))
 
 ggmap1
