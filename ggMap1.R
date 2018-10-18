d24401d26392d683ecf979dbb648847f22ebb23a
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-18 02:23:37 -0400

    Saving ggMap1.R

--- ggMap1.R
+++ ggMap1.R
@@ -11,7 +11,8 @@ us<- map_data("state")
 ggmap1 <- ggplot(mergeDF, aes(map_id = stateName))  + geom_map(map= us, aes(fill=stateArea))
 
 #Defining the x and y axis values of the map 
-#coord_map balances the distortion and aspect ratio of the ggmap1#ggtitle()
+#coord_map balances the distortion and aspect ratio of the ggmap1
+#ggtitle() gives a title to the map
 ggmap1 <- ggmap1 + expand_limits(x = mergeDF$x, y=mergeDF$y) + coord_map() + ggtitle("Map of US based on state area")
 
 
