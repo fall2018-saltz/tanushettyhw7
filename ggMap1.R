b4bd727bd616d88921fa848fd7a7c33db369333f
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-18 02:22:22 -0400

    Saving ggMap1.R

--- ggMap1.R
+++ ggMap1.R
@@ -7,9 +7,10 @@ library("ggmap")
 us<- map_data("state")
 
 #Creating a ggplot object and passing mergeDF as the input with map ID as the statename
+#Creating a map visualization withgeom_map
 ggmap1 <- ggplot(mergeDF, aes(map_id = stateName))  + geom_map(map= us, aes(fill=stateArea))
 
-
+#Defining the x and y axis values of the map
 ggmap1 <- ggmap1 + expand_limits(x = mergeDF$x, y=mergeDF$y) + coord_map() + ggtitle("Map of US based on state area")
 
 ggmap1
