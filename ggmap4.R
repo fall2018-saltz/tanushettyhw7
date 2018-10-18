03b19a2a542f7c36d4e1dcdaa4868bf25019330b
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-18 01:19:32 -0400

    Saving ggmap4.R

--- ggmap4.R
+++ ggmap4.R
@@ -10,4 +10,6 @@ x1 <- latlon$lat-10
 x2 <- latlon$lat+10
 y2 <- latlon$lat+10
 
-ggmap4 <- ggplot(mergeDF,aes(map_id = stateName))
+ggmap4 <- ggplot(mergeDF,aes(map_id = stateName))+ geom_map(map=us,(fill=Murder))
+
+ggmap4 <- ggmap4 + scale_fill_gradient(low="white",high="red")+xlim(c(x1,x2))+ylim(c(y1,y2))
