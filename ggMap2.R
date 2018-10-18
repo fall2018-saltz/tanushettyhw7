bebdacfcd95779ef76930fd0c012f070eadfdf32
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-18 01:38:21 -0400

    Saving ggMap2.R

--- ggMap2.R
+++ ggMap2.R
@@ -6,11 +6,11 @@ us <- map_data("state")
 
 map2 <- ggplot(mergeDF, aes(map_id= stateName))
 
-map2 <- map2 +geom_map(map =us, aes(fill = Murder))
+ggmap2 <- ggmap2 +geom_map(map =us, aes(fill = Murder))
 
-map2 <- map2+ expand_limits(x = mergeDF$x, y=mergeDF$y)
+ggmap2 <- ggmap2+ expand_limits(x = mergeDF$x, y=mergeDF$y)
 
-map2 <- map2 + coord_map() + ggtitle("Map of US based on murder rate of states")
+ggmap2 <- ggmap2 + coord_map() + ggtitle("Map of US based on murder rate of states")
 
-map2
+ggmap2
 
