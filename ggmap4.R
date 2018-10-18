e99eed8f12c2a8dd8993fd1b42edccf7bd2f4475
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-18 01:16:28 -0400

    Saving ggmap4.R

--- ggmap4.R
+++ ggmap4.R
@@ -6,4 +6,6 @@ us <- map_data("stage")
 
 latlon <- geocode("new york city,ny", source="dsk")
 y1 <- latlon$lat-10
-y1 <- latlon$lat-10
+x1 <- latlon$lat-10
+x2 <- latlon$lat+10
+y2 <- latlon$lat+10
