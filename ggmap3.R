a53231b99b51339269cb72a0c64afeaba6159bba
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-18 02:30:02 -0400

    Saving ggmap3.R

--- ggmap3.R
+++ ggmap3.R
@@ -10,6 +10,7 @@ us <- map_data("state")
 ggmap3 <- ggplot(mergeDF, aes(map_id = stateName)) 
 
 #Defining the x and y axis values of the map and 
+#coord_map balances the distortion and the aspect ratio of the map
 ggmap3 <- ggmap3 + geom_map(map = us, aes(fill=Murder))+ scale_fill_gradient(low="white",high="red")
 
 ggmap3 <- ggmap3 + expand_limits(x=us$long, y=us$lat) + coord_map()
