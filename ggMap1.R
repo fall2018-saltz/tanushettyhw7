f1dffa8506b7fb10f6bddce473ff056b82c4d008
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-18 00:25:59 -0400

    moving Map1.R to ggMap1.R

---
+++ ggMap1.R
@@ -0,0 +1,16 @@
+
+library("ggplot2")
+library("ggmap")
+
+us<- map_data("state")
+
+
+map1 <- ggplot(mergeDF, aes(map_id = stateName)
+
+
+map1 <- map1 + geom_map(map=us, aes(fill=stateName)
+
+map1 <- map1 + expand_limits(x = mergeDF$x, y=mergeDF$y)
+
+map1 <- map1 + coord_map() + ggtitle("Map of US based on state area")
+map1
