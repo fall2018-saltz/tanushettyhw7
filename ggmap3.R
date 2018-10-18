bd8bf154b10187162f3d8c4a637ba865abf7c5d5
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-18 02:29:04 -0400

    Saving ggmap3.R

--- ggmap3.R
+++ ggmap3.R
@@ -6,6 +6,7 @@ library("ggmap")
 #Converting data from the maps package into a data frame suitable for plotting with ggplot2
 us <- map_data("state")
 
+#Creating a map visualization 
 ggmap3 <- ggplot(mergeDF, aes(map_id = stateName)) 
 
 ggmap3 <- ggmap3 + geom_map(map = us, aes(fill=Murder))+ scale_fill_gradient(low="white",high="red")
