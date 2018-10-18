21a50b9aee6b0cad7628bb2b58eecb0ba93ebe8a
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-18 11:17:51 -0400

    Saving ggmap4.R

--- ggmap4.R
+++ ggmap4.R
@@ -12,7 +12,7 @@ x2 <- latlon$lon+10
 y2 <- latlon$lat+10
 
 
-ggmap4 <- ggplot(mergeDF,aes(map_id = stateName))+ geom_map(map=us, color="white")
+ggmap4 <- ggplot(mergeDF,aes(map_id = stateName))+ geom_map(map=us, color="black", fill="white")
 ggmap4 <- ggmap4 + xlim(c(x1,x2))+ylim(c(y1,y2))
 ggmap4 <- ggmap4 + expand_limits(x=us$long,y=us$lat)+coord_map()
 #size attribute controls the size of the points
