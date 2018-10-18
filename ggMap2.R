7bb75e53284cad8328c22c5cd35418f3b6abf842
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-18 02:25:54 -0400

    Saving ggMap2.R

--- ggMap2.R
+++ ggMap2.R
@@ -9,6 +9,7 @@ ggmap2 <- ggplot(mergeDF, aes(map_id= stateName))
 #Creating a map visualiation based on murder rate of each state
 ggmap2 <- ggmap2 +geom_map(map =us, aes(fill = Murder))
 
+#defining the x and y axis values of the map
 ggmap2 <- ggmap2+ expand_limits(x = mergeDF$x, y=mergeDF$y)
 
 ggmap2 <- ggmap2 + coord_map() + ggtitle("Map of US based on murder rate of states")
