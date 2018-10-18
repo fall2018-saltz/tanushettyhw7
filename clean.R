94c096db724beacb770c17d4a263cbf6eb23ead0
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-18 02:09:21 -0400

    Saving clean.R

--- clean.R
+++ clean.R
@@ -3,7 +3,8 @@ clean_data <- function(states){
 #Cleaning the data. Removing the first and the last row
     states <- states[,-c(1:4)]
     states <- states[c(-1,-53),]
-    
+ 
+ #Using colnames() to replace the columns in the dataframe   
     colnames(states)[c(1,2,3,4)] <- c("stateName","population","popOver18","percentOver18")
     return(states)
 }
