db7f46c89ae788086b30ddaf46229fdbb9dfc8a0
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-18 00:02:15 -0400

    Saving mergingDataframe.R

--- mergingDataframe.R
+++ mergingDataframe.R
@@ -6,4 +6,4 @@ rownames(clean_data) <- as.vector(clean_data[,"stateName"])
 mergeDataframe <- merge(clean_data,arrests, by="row.names")
 mergeDataframe
 
-stateName <- state.stateName
+stateName <- state.names
